34f1189d4d4ba7201449fbb73564d6b960dccfa8
fabio <>
2025-03-06 15:24:00 -0300

    refactor to not user venv anymore

--- install.sh
+++ install.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-git clone https://github.com/fabioalmeida08/bootstrap-cli.git
-cd bootstrap-cli
 
-python3 -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-deactivate
+# python3 -m venv .venv
+# source .venv/bin/activate
+# pip install --upgrade pip
+# pip install -r requirements.txt
+# deactivate
 
 touch bootstrap.sh
 script_dir_abs="$(cd "$script_dir" && pwd)"
 cat <<EOF > bootstrap.sh
 #!/bin/bash
-source $script_dir_abs/.venv/bin/activate
 python3 $script_dir_abs/main.py "\$@"
-deactivate
 EOF
 
 chmod +x bootstrap.sh
